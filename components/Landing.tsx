3990d1215bf73dfcc67f20ecb1413c49a0f47d40
davieoba <bodunrindavidbond@gmail.com>
2022-09-18 20:54:41 +0100

    working on the landing page

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,14 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
+import { Button } from './Button'
 
 export const Landing = () => {
   return (
-    <section>
-      <div>
-        <h1 className='space-y-3 text-5xl font-semibold tracking-wide lg:text-5xl'>
+    <section className='sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8'>
+      <div className='space-y-8'>
+        <h1 className='space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl'>
           <span className='block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>Powered</span>
           <span className='block'>By Intellect</span>
           <span className='block'>Driven By values</span>
         </h1>
+
+        <div>
+          <Button title='Buy now' />
+          <a href="#" className='link'>learn more</a>
+        </div>
+      </div>
+
+      <div className='relative hidden h-[450px] w-[450px] transition-all duration-500 md:inline lg:h-[650px] lg:w-[600px]'>
+        <Image src='/iphone.png' layout='fill' objectFit='contain' />
       </div>
     </section>
   )
