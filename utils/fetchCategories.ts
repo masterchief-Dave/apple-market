72b85532ada15f9538bf4ea0fad96449a350a7e5
Bodunrin David <bodunrindavidbond@gmail.com>
2022-09-20 07:48:49 +0100

    added cors origin and use firefox for sanity studio

--- utils/fetchCategories.ts
+++ utils/fetchCategories.ts
@@ -1,3 +1,6 @@
 export const fetchCategories = async () => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getCategories`)
+
+  const data = await response.json()
+  console.log(data)
 }
