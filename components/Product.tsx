86169ae5395af35ac5aefd472242351d132502c6
David Bodunrin <david@ecscorpresources.com>
2022-09-26 09:00:41 +0100

    including global state management to the project

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,3 +1,4 @@
+import { ShoppingCartIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
 import React from 'react'
 import { urlFor } from '../sanity'
@@ -7,20 +8,29 @@ interface Props {
 }
 
 export const Product = ({ product }: Props) => {
-  console.log(product.image)
-  // const productImage = product.image
-  // console.log(productImage)
+  const addItemToBasket = () => {
+    // some code
+  }
+
   return (
     <div className='flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383c] md:w-[400px] md:p-10'>
       <div className='relative h-64 w-full md:h-72'>
         <Image src={urlFor(product.image).width(200).url()} alt='product' layout='fill' objectFit='contain' />
       </div>
+
+      <div className="flex flex-1 items-center justify-between space-x-3">
+        <div className="space-y-2 text-xl text-white md:text-2xl">
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+        </div>
+
+        <div
+          className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] md:w-[70px]"
+          onClick={addItemToBasket}
+        >
+          <ShoppingCartIcon className="h-8 w-8 text-white" />
+        </div>
+      </div>
     </div>
   )
 }
-{/* <img src={urlFor(person.image).width(200).url()} /> */ }
-
-/*
-{urlFor(product.image[0]).url()}
-
-*/
---
+++ redux/basketSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Props {
+  name: ''
+}
+const initialState = []
+
+const basketSlice = {
+  name: 'basket',
+  initialState,
+  reducers: {}
+}
+
+export default basketSlice.reducers
